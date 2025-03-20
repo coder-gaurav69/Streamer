bbcb07610ffe7fdb2f5219407466b7b00e435896
coder-gaurav69 <gauravchand222@gmail.com>
2025-03-20 10:24:32 +0530

    no

--- frontend/src/Context/SocketContext.jsx
+++ frontend/src/Context/SocketContext.jsx
@@ -4,17 +4,10 @@ import { io } from "socket.io-client";
 const SocketContext = createContext(null);
 
 const SocketProvider = ({ children }) => {
-  const socket = useRef(null);
-
-  useEffect(() => {
-    socket.current = io(process.env.REACT_APP_BACKEND_URL, {
-      transports: ["websocket"], // Ensures a stable connection
-    });
-
-    return () => {
-      socket.current.disconnect(); // Cleanup on unmount
-    };
-  }, []);
+    const socket = useRef(null)
+    useEffect(()=>{
+      socket.current = io("http://localhost:5000")
+    },[])
 
   return (
     <SocketContext.Provider value={socket}>
