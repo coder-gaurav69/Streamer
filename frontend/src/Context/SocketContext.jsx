3a7ebf4cd0b7fa6f92c6360683da3827e0346249
coder-gaurav69 <167112669+coder-gaurav69@users.noreply.github.com>
2025-03-20 10:30:53 +0530

    Update SocketContext.jsx

--- frontend/src/Context/SocketContext.jsx
+++ frontend/src/Context/SocketContext.jsx
@@ -1,13 +1,20 @@
-import React, { createContext, useEffect, useRef, useState } from "react";
+import React, { createContext, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
 const SocketContext = createContext(null);
 
 const SocketProvider = ({ children }) => {
-    const socket = useRef(null)
-    useEffect(()=>{
-      socket.current = io("http://localhost:5000")
-    },[])
+  const socket = useRef(null);
+
+  useEffect(() => {
+    socket.current = io(process.env.REACT_APP_BACKEND_URL, {
+      transports: ["websocket"], // Ensures a stable connection
+    });
+
+    return () => {
+      socket.current.disconnect(); // Cleanup on unmount
+    };
+  }, []);
 
   return (
     <SocketContext.Provider value={socket}>
